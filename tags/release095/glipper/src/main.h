7f11c0c98d30a1a8cba4be34e7d20d79c3b759fc
nobody <nobody@d99fc9e9-cf59-3981-239e-a3df05a56915>
2006-11-12 16:25:12 +0000

    This commit was manufactured by cvs2svn to create tag 'release095'.

---
+++ tags/release095/glipper/src/main.h
@@ -0,0 +1,13 @@
+//extern variables:
+extern int maxElements; 
+extern int maxItemLength;
+extern gboolean usePrimary; 
+extern gboolean useDefault; 
+extern gboolean markDefault; 
+extern gboolean weSaveHistory;
+extern char* keyComb;
+
+//extern functions:
+void savePreferences();
+void applyPreferences();
+void unbindKey();
