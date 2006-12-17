fee99379d23cad870ba11fd6cc919175a0ebd3ef
svenrech <svenrech@d99fc9e9-cf59-3981-239e-a3df05a56915>
2006-12-17 00:38:51 +0000

    added dialog for plugin management

--- glipper/plugins/newline.py
+++ glipper/plugins/newline.py
@@ -2,3 +2,8 @@ import glipper
 
 def newItem(arg):
 	glipper.setItem(0, glipper.getItem(0)+'\n')
+
+def getInfo():
+	info = {"Name": "New Line",
+		"Description": "This plugin adds a newline character at the end of items in the clipboard"}
+	return info
---
+++ glipper/src/plugin-dialog.h
@@ -0,0 +1,2 @@
+
+void showPluginDialog(gpointer data);
