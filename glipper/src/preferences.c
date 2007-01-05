4efe7a05909f0077b8ee30ec7d994a8066c31060
svenrech <svenrech@d99fc9e9-cf59-3981-239e-a3df05a56915>
2007-01-05 21:28:04 +0000

    added gconf patch

--- glipper/src/preferences.c
+++ glipper/src/preferences.c
@@ -5,10 +5,17 @@
 #include <gtk/gtk.h>
 #include <glade/glade.h>
 #include "preferences.h"
-#include "main.h"
 #include "utils/glipper-i18n.h"
 
 #define GLADE_XML_FILE "glipper-properties.glade"
+
+static GConfClient* global_conf;
+
+extern int hasChanged;
+extern GSList* history;
+extern void keyhandler(char *keystring, gpointer user_data);
+extern GtkTooltips* toolTip; //The trayicon's tooltip
+extern GtkWidget* eventbox; //The trayicon's eventbox
 
 GtkWidget* historyLength;
 GtkWidget* itemLength;
@@ -20,52 +27,189 @@ GtkWidget* keyCombEntry;
 GtkWidget* applyButton;
 GtkWidget* prefWin;
 GtkWidget* helpButton;
+GtkWidget* closeButton;
+
+//Callbacks needed to update the GUI and history after a key change
+
+void
+max_item_length_key_changed_callback(GConfClient *client,
+		    guint        cnxn_id,
+		    GConfEntry  *entry,
+		    gpointer     user_data)
+{
+	hasChanged = 1;
+	GConfValue *value = gconf_entry_get_value (entry);
+	gtk_spin_button_set_value((GtkSpinButton*)itemLength, gconf_value_get_int(value));
+}
+
+void
+max_elements_key_changed_callback(GConfClient *client,
+		    guint        cnxn_id,
+		    GConfEntry  *entry,
+		    gpointer     user_data)
+{
+	hasChanged = 1;
+	GConfValue *value = gconf_entry_get_value (entry);
+	GSList* deleteElement = g_slist_nth(history, gconf_value_get_int(value)-1);
+	if (deleteElement != NULL)
+	{
+		g_slist_free(deleteElement->next);
+		deleteElement->next = NULL;
+	}
+	gtk_spin_button_set_value((GtkSpinButton*)historyLength, gconf_value_get_int(value));
+}
+
+void
+use_primary_clipboard_key_changed_callback(GConfClient *client,
+		    guint        cnxn_id,
+		    GConfEntry  *entry,
+		    gpointer     user_data)
+{
+	GConfValue *value = gconf_entry_get_value(entry);
+	gtk_toggle_button_set_active((GtkToggleButton*)primaryCheck, gconf_value_get_bool(value));
+}
+								
+void
+use_default_clipboard_key_changed_callback(GConfClient *client,
+		    guint        cnxn_id,
+		    GConfEntry  *entry,
+		    gpointer     user_data)
+{
+	GConfValue *value = gconf_entry_get_value(entry);
+	gtk_toggle_button_set_active((GtkToggleButton*)defaultCheck, gconf_value_get_bool(value));
+}
+		
+void
+mark_default_entry_key_changed_callback(GConfClient *client,
+		    guint        cnxn_id,
+		    GConfEntry  *entry,
+		    gpointer     user_data)
+{
+	hasChanged = 1;
+	GConfValue *value = gconf_entry_get_value(entry);
+	gtk_toggle_button_set_active((GtkToggleButton*)markDefaultCheck, gconf_value_get_bool(value));
+}
+
+void
+save_history_key_changed_callback(GConfClient *client,
+		    guint        cnxn_id,
+		    GConfEntry  *entry,
+		    gpointer     user_data)
+{
+	GConfValue *value = gconf_entry_get_value(entry);
+	gtk_toggle_button_set_active((GtkToggleButton*)saveHistCheck, gconf_value_get_bool(value));
+}
+
+void
+key_combination_key_changed_callback(GConfClient *client,
+		    guint        cnxn_id,
+		    GConfEntry  *entry,
+		    gpointer     user_data)
+{
+	hasChanged = 1;
+	GConfValue *value = gconf_entry_get_value(entry);
+	keybinder_bind(gconf_value_get_string(value), keyhandler, NULL);
+	gtk_tooltips_set_tip(toolTip, eventbox, g_strdup_printf(_("Glipper (%s)\nClipboardmanager"),
+						gconf_value_get_string(value)), "Glipper");
+	gtk_entry_set_text((GtkEntry*)keyCombEntry, gconf_value_get_string(value));
+}
 
 //Sets initial state of widgets
 void setWidgets()
 {
-	gtk_spin_button_set_value((GtkSpinButton*)historyLength, maxElements);
-	gtk_spin_button_set_value((GtkSpinButton*)itemLength, maxItemLength);
-	gtk_toggle_button_set_active((GtkToggleButton*)primaryCheck, usePrimary);
-	gtk_toggle_button_set_active((GtkToggleButton*)defaultCheck, useDefault);
-	gtk_toggle_button_set_active((GtkToggleButton*)markDefaultCheck, markDefault);
-	gtk_toggle_button_set_active((GtkToggleButton*)saveHistCheck, weSaveHistory);
-	gtk_entry_set_text((GtkEntry*)keyCombEntry, keyComb);
-	on_clipCheck_toggled(NULL, NULL); //can make markDefaultCheck possibly unsensitive
+	gtk_spin_button_set_value((GtkSpinButton*)historyLength, gconf_client_get_int(global_conf, MAX_ELEMENTS_KEY, NULL));
+	gtk_spin_button_set_value((GtkSpinButton*)itemLength, gconf_client_get_int(global_conf, MAX_ITEM_LENGTH_KEY, NULL));
+	gtk_toggle_button_set_active((GtkToggleButton*)primaryCheck, gconf_client_get_bool(global_conf, USE_PRIMARY_CLIPBOARD_KEY, NULL));
+	gtk_toggle_button_set_active((GtkToggleButton*)defaultCheck, gconf_client_get_bool(global_conf, USE_DEFAULT_CLIPBOARD_KEY, NULL));
+	gtk_toggle_button_set_active((GtkToggleButton*)markDefaultCheck, gconf_client_get_bool(global_conf, MARK_DEFAULT_ENTRY_KEY, NULL));
+	gtk_toggle_button_set_active((GtkToggleButton*)saveHistCheck, gconf_client_get_bool(global_conf, SAVE_HISTORY_KEY, NULL));
+	gtk_entry_set_text((GtkEntry*)keyCombEntry, gconf_client_get_string(global_conf, KEY_COMBINATION_KEY, NULL));
+	on_defaultCheck_toggled((GtkToggleButton*)defaultCheck,NULL);
+}
+
+//Every widget needs to update the key in the GConf database when he changes his status
+void
+on_itemLength_value_changed                   (GtkSpinButton       *spinButton,
+                                        		gpointer         user_data)
+{
+	hasChanged = 1;
+	gint value;
+	value = gtk_spin_button_get_value (spinButton);
+	gconf_client_set_int (global_conf, MAX_ITEM_LENGTH_KEY, value, NULL);
+}
+
+void
+on_historyLength_value_changed                   (GtkSpinButton       *spinButton,
+                                        		gpointer         user_data)
+{
+	hasChanged = 1;
+	gint value;
+	value = gtk_spin_button_get_value (spinButton);
+	gconf_client_set_int (global_conf, MAX_ELEMENTS_KEY, value, NULL);
+}
+
+void
+on_keyCombEntry_changed                   (GtkEntry       *entry,
+                                        		gpointer         user_data)
+{
+	hasChanged = 1;
+	const gchar* value = gtk_entry_get_text (entry);
+	gconf_client_set_string (global_conf, KEY_COMBINATION_KEY, value, NULL);
 }
 
 //Set markDefaultCheck checkbox (ctrl+c in blue) active or not
 void
-on_clipCheck_toggled                   (GtkToggleButton *togglebutton,
+on_primaryCheck_toggled                   (GtkToggleButton *toggleButton,
+                                        gpointer         user_data)
+{
+	gint value;
+	value = gtk_toggle_button_get_active (toggleButton);
+	gconf_client_set_bool (global_conf, USE_PRIMARY_CLIPBOARD_KEY, value, NULL);
+}
+
+void
+on_defaultCheck_toggled                   (GtkToggleButton *toggleButton,
                                         gpointer         user_data)
 {
 	//Only permit to distinguish ctrl+c entry if both types of clipboard are present
 	if (!(gtk_toggle_button_get_active((GtkToggleButton*)primaryCheck) && 
-	  gtk_toggle_button_get_active((GtkToggleButton*)defaultCheck)))
+	  gtk_toggle_button_get_active((GtkToggleButton*)toggleButton)))
 		gtk_widget_set_sensitive(markDefaultCheck, FALSE);
 	else
 		gtk_widget_set_sensitive(markDefaultCheck, TRUE);
+	gint value;
+	value = gtk_toggle_button_get_active (toggleButton);
+	gconf_client_set_bool (global_conf, USE_DEFAULT_CLIPBOARD_KEY, value, NULL);
 }
 
-//Save preferences when window is closed via apply button
 void
-on_applyButton_clicked                 (GtkButton       *button,
+on_markDefaultCheck_toggled                   (GtkToggleButton *toggleButton,
                                         gpointer         user_data)
 {
-	unbindKey();
-	maxElements = gtk_spin_button_get_value_as_int((GtkSpinButton*)historyLength); 
-	maxItemLength = gtk_spin_button_get_value_as_int((GtkSpinButton*)itemLength); 
-	usePrimary = gtk_toggle_button_get_active((GtkToggleButton*)primaryCheck);
-	useDefault = gtk_toggle_button_get_active((GtkToggleButton*)defaultCheck);
-	markDefault = gtk_toggle_button_get_active((GtkToggleButton*)markDefaultCheck);
-	weSaveHistory = gtk_toggle_button_get_active((GtkToggleButton*)saveHistCheck);
-	keyComb = g_strdup(gtk_entry_get_text((GtkEntry*)keyCombEntry));
-	savePreferences();
-	applyPreferences();
+	hasChanged = 1;
+	gint value;
+	value = gtk_toggle_button_get_active (toggleButton);
+	gconf_client_set_bool (global_conf, MARK_DEFAULT_ENTRY_KEY, value, NULL);
+}
+
+void
+on_saveHistCheck_toggled                   (GtkToggleButton *toggleButton,
+                                        gpointer         user_data)
+{
+	gint value;
+	value = gtk_toggle_button_get_active (toggleButton);
+	gconf_client_set_bool (global_conf, SAVE_HISTORY_KEY, value, NULL);
+}
+
+void
+on_closeButton_clicked                 (GtkButton       *button,
+                                        gpointer         user_data)
+{
 	gtk_widget_destroy(prefWin);
 }
 
-on_helpButton_clicked                 (GtkButton       *button,
+
+void on_helpButton_clicked                 (GtkButton       *button,
                                         gpointer         user_data)
 {
 #ifndef DISABLE_GNOME
@@ -105,28 +249,97 @@ void showPreferences(gpointer data)
 	markDefaultCheck = glade_xml_get_widget(gladeWindow, "markDefaultCheck");
 	saveHistCheck = glade_xml_get_widget(gladeWindow, "saveHistCheck");
 	keyCombEntry = glade_xml_get_widget(gladeWindow, "keyComb");
-	applyButton = glade_xml_get_widget(gladeWindow, "applyButton");
 	helpButton = glade_xml_get_widget(gladeWindow, "helpButton");
+	closeButton = glade_xml_get_widget(gladeWindow, "closeButton");
 	prefWin = glade_xml_get_widget(gladeWindow, "preferences-dialog");
 
 	//Connect signals to handlers
-	g_signal_connect_after ((gpointer) primaryCheck, "toggled",
-		G_CALLBACK (on_clipCheck_toggled),
+	g_signal_connect ((gpointer) primaryCheck, "toggled",
+		G_CALLBACK (on_primaryCheck_toggled),
                           NULL);
-	g_signal_connect_after ((gpointer) defaultCheck, "toggled",
-		G_CALLBACK (on_clipCheck_toggled),
-		NULL);
-	g_signal_connect ((gpointer) applyButton, "clicked",
-		G_CALLBACK (on_applyButton_clicked),
+	g_signal_connect ((gpointer) defaultCheck, "toggled",
+		G_CALLBACK (on_defaultCheck_toggled),
 		NULL);
 	g_signal_connect ((gpointer) helpButton, "clicked",
 		G_CALLBACK (on_helpButton_clicked),
 		NULL);
-
+	g_signal_connect ((gpointer) closeButton, "clicked",
+		G_CALLBACK (on_closeButton_clicked),
+		NULL);
+	g_signal_connect ((gpointer) itemLength, "value_changed",
+		G_CALLBACK (on_itemLength_value_changed),
+		NULL);
+	g_signal_connect ((gpointer) historyLength, "value_changed",
+		G_CALLBACK (on_historyLength_value_changed),
+		NULL);
+	g_signal_connect ((gpointer) markDefaultCheck, "toggled",
+		G_CALLBACK (on_markDefaultCheck_toggled),
+		NULL);
+	g_signal_connect ((gpointer) saveHistCheck, "toggled",
+		G_CALLBACK (on_saveHistCheck_toggled),
+		NULL);
+	g_signal_connect ((gpointer) keyCombEntry, "changed",
+		G_CALLBACK (on_keyCombEntry_changed),
+		NULL);
 	//Show preferences dialog
 	setWidgets();
 	gtk_widget_show_all(prefWin);
 
 	//free the glade data
 	g_object_unref(gladeWindow);
+}
+
+void initPreferences(GConfClient* conf)
+{
+	g_return_if_fail (conf != NULL);
+	g_return_if_fail (global_conf == NULL);
+
+	global_conf = conf;
+	g_object_ref (G_OBJECT (global_conf));
+	
+	gconf_client_add_dir (conf, PATH,
+                           GCONF_CLIENT_PRELOAD_NONE,
+                           NULL);
+
+	gconf_client_notify_add (conf,
+                           MAX_ITEM_LENGTH_KEY,
+                           max_item_length_key_changed_callback,
+                           NULL,
+                           NULL, NULL);
+
+	gconf_client_notify_add (conf,
+                           MAX_ELEMENTS_KEY,
+                           max_elements_key_changed_callback,
+                           NULL,
+                           NULL, NULL);
+
+	gconf_client_notify_add (conf,
+                           USE_PRIMARY_CLIPBOARD_KEY,
+                           use_primary_clipboard_key_changed_callback,
+                           NULL,
+                           NULL, NULL);
+
+	gconf_client_notify_add (conf,
+                           USE_DEFAULT_CLIPBOARD_KEY,
+                           use_default_clipboard_key_changed_callback,
+                           NULL,
+                           NULL, NULL);
+
+	gconf_client_notify_add (conf,
+                           MARK_DEFAULT_ENTRY_KEY,
+                           mark_default_entry_key_changed_callback,
+                           NULL,
+                           NULL, NULL);
+                           
+	gconf_client_notify_add (conf,
+                           SAVE_HISTORY_KEY,
+                           save_history_key_changed_callback,
+                           NULL,
+                           NULL, NULL);
+                           
+	gconf_client_notify_add (conf,
+                           KEY_COMBINATION_KEY,
+                           key_combination_key_changed_callback,
+                           NULL,
+                           NULL, NULL);
 }
--- glipper/src/preferences.h
+++ glipper/src/preferences.h
@@ -1,5 +1,16 @@
 #include <gtk/gtk.h>
+#include <gconf/gconf-client.h>
 
+#define PATH "/apps"
+#define KEY_PREFIX PATH "/glipper"
+#define MAX_ELEMENTS_KEY KEY_PREFIX "/max_elements"
+#define MAX_ITEM_LENGTH_KEY KEY_PREFIX "/max_item_length"
+#define USE_PRIMARY_CLIPBOARD_KEY KEY_PREFIX "/use_primary_clipboard"
+#define USE_DEFAULT_CLIPBOARD_KEY KEY_PREFIX "/use_default_clipboard"
+#define MARK_DEFAULT_ENTRY_KEY KEY_PREFIX "/mark_default_entry"
+#define SAVE_HISTORY_KEY KEY_PREFIX "/save_history"
+#define CHECK_INTERVAL_KEY KEY_PREFIX "/check_interval"
+#define KEY_COMBINATION_KEY KEY_PREFIX "/key_combination"
 
 void
 on_DefaultCheck_toggled                (GtkToggleButton *togglebutton,
@@ -9,13 +20,72 @@ void
 showPreferences                        (gpointer data);
 
 void
-on_DefaultCheck_toggled                (GtkToggleButton *togglebutton,
+on_primaryCheck_toggled                   (GtkToggleButton *togglebutton,
                                         gpointer         user_data);
 
 void
-on_clipCheck_toggled                   (GtkToggleButton *togglebutton,
+on_defaultCheck_toggled                   (GtkToggleButton *togglebutton,
                                         gpointer         user_data);
 
 void
 on_closeButton_clicked                 (GtkButton       *button,
                                         gpointer         user_data);
+
+void
+on_helpButton_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_itemLength_value_changed            (GtkSpinButton       *spinButton,
+                                        gpointer         user_data);
+
+void
+on_historyLength_value_changed            (GtkSpinButton       *spinButton,
+                                        gpointer         user_data);
+    
+void
+on_markDefaultCheck_toggled            (GtkToggleButton *toggleButton,
+                                        gpointer         user_data);
+
+void
+on_saveHistCheck_toggled            (GtkToggleButton *toggleButton,
+                                        gpointer         user_data);
+
+void
+on_keyCombEntry_changed            (GtkEntry *entry,
+                                        gpointer         user_data);
+                                        
+void max_item_length_key_changed_callback(GConfClient *client,
+		    guint        cnxn_id,
+		    GConfEntry  *entry,
+		    gpointer     user_data);
+
+void max_elements_key_changed_callback(GConfClient *client,
+		    guint        cnxn_id,
+		    GConfEntry  *entry,
+		    gpointer     user_data);
+
+void use_primary_clipboard_key_changed_callback(GConfClient *client,
+		    guint        cnxn_id,
+		    GConfEntry  *entry,
+		    gpointer     user_data);
+										
+void use_default_clipboard_key_changed_callback(GConfClient *client,
+		    guint        cnxn_id,
+		    GConfEntry  *entry,
+		    gpointer     user_data);
+										
+void mark_default_entry_key_changed_callback(GConfClient *client,
+		    guint        cnxn_id,
+		    GConfEntry  *entry,
+		    gpointer     user_data);
+										
+void save_history_key_changed_callback(GConfClient *client,
+		    guint        cnxn_id,
+		    GConfEntry  *entry,
+		    gpointer     user_data);
+										
+void key_combination_key_changed_callback(GConfClient *client,
+		    guint        cnxn_id,
+		    GConfEntry  *entry,
+		    gpointer     user_data);
