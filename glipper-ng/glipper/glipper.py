b389e2c0e0c6da0b035c11b8a358c0d2b600aedc
svenrech <svenrech@d99fc9e9-cf59-3981-239e-a3df05a56915>
2007-09-30 19:10:15 +0000

    fixed headers

--- glipper-ng/glipper/glipper.py
+++ glipper-ng/glipper/glipper.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 
-/* Glipper - Clipboardmanager for GNOME
- * Copyright (C) 2007 Glipper Team
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- */
+# Glipper - Clipboardmanager for GNOME
+# Copyright (C) 2007 Glipper Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+#
 
 import gobject
 gobject.threads_init()
