3978a842113170966134c5b00ad4bc5b29d5b084
svenrech <svenrech@d99fc9e9-cf59-3981-239e-a3df05a56915>
2007-09-30 18:24:23 +0000

    Added LGPL Headers to source files

--- trunk/glipper-ng/glipper/plugins/newline.py
+++ trunk/glipper-ng/glipper/plugins/newline.py
@@ -1,3 +1,22 @@
+/* Glipper - Clipboardmanager for GNOME
+ * Copyright (C) 2007 Glipper Team
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 #This is a small example plugin, which just adds a newline character at the end of every new clipboardsentry line
 #
 #Please be aware that this plugin doesn't work very well, due to the very strange implementation of clipboard
